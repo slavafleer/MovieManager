73c657a615df2ed64aaae1b9309eed95dd3cc767
slavafleer <slavafleer@gmail.com>
2015-12-26 18:09:54 +0200

    finished custom item chages

--- app/src/main/java/com/slavafleer/moviemanager/adapter/MainListAdapter.java
+++ app/src/main/java/com/slavafleer/moviemanager/adapter/MainListAdapter.java
@@ -2,26 +2,38 @@ package com.slavafleer.moviemanager.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.slavafleer.moviemanager.R;
+import com.slavafleer.moviemanager.asynctask.OMDbImageDownloaderAsyncTask;
 import com.slavafleer.moviemanager.data.Movie;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
  * Custom adapter for list view in Main Activity.
  */
-public class MainListAdapter extends ArrayAdapter<Movie> {
+public class MainListAdapter extends ArrayAdapter<Movie>
+        implements OMDbImageDownloaderAsyncTask.Callbacks {
 
     private Activity mActivity;
     private ArrayList<Movie> mMovies;
     private LayoutInflater mLayoutInflater;
+    private ProgressBar mProgressBar;
+    private ImageView mImageViewPoster;
 
     public MainListAdapter(Activity activity, ArrayList<Movie> movies) {
         super(activity, 0, movies);
@@ -33,7 +45,8 @@ public class MainListAdapter extends ArrayAdapter<Movie> {
                 Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    // This function will be called for each item in the list, during the list creation:
+    // This function will be called for each item in the list, during the list creation
+    // and filling all Views with relevant data.
     public View getView(int position, View convertView, ViewGroup parent) {
         Movie movie = mMovies.get(position);
         View itemLayout = mLayoutInflater.inflate(R.layout.adapter_main_listview_item, null);
@@ -79,7 +92,75 @@ public class MainListAdapter extends ArrayAdapter<Movie> {
                 color = ContextCompat.getColor(mActivity, R.color.colorRatingTransparent);
                 break;
         }
-        textViewTitle.setBackgroundColor(color);
+//        textViewTitle.setBackgroundColor(color);
+        LinearLayout linearLayoutRoot = (LinearLayout)
+                itemLayout.findViewById(R.id.linearLaoutItemRoot);
+        linearLayoutRoot.setBackgroundColor(color);
+
+        RatingBar ratingBar = (RatingBar) itemLayout.findViewById(R.id.ratingBarItem);
+        /** Rating Bar Bug!!!
+         * Next code prevents bug of filling whole star even if rating data is integer.
+         * It happens on some "old" apis as 4.2.2 and 4.3, maybe more.
+         * On api 5.* it's not needed, cause have no such bug.
+         * The prevent this bug, need to rewrite rating data again with some change.
+         * It must be on same star!!!
+         * */
+        float ratingFloat = rating / 2f;
+        if((ratingFloat * 10) % 10 != 0 ) {
+            ratingBar.setRating(ratingFloat + 0.5f);
+        }
+        ratingBar.setRating(ratingFloat);
+
+        boolean isWatched = movie.getIsWatched();
+        ImageView imageViewIsWatched = (ImageView)
+                itemLayout.findViewById(R.id.imageViewItemIsWatched);
+        if(isWatched) {
+            imageViewIsWatched.setVisibility(View.VISIBLE);
+        } else {
+            imageViewIsWatched.setVisibility(View.INVISIBLE);
+        }
+
+        // Getting poster from internet.
+        mImageViewPoster = (ImageView) itemLayout.findViewById(R.id.imageViewItemPoster);
+        mProgressBar = (ProgressBar) itemLayout.findViewById(R.id.progressBarItem);
+        OMDbImageDownloaderAsyncTask omDbImageDownloaderAsyncTask =
+                new OMDbImageDownloaderAsyncTask(this);
+
+        try {
+            URL url = new URL(movie.getUrl());
+            omDbImageDownloaderAsyncTask.execute(url);
+        } catch (MalformedURLException e) {
+            mImageViewPoster.setImageResource(R.drawable.movie_clapper_icon);
+        }
+
         return itemLayout;
+    }
+
+    // Actions while OMDbImageDownloaderAsyncTask is created.
+    // Do it before new thread creating.
+    public void onAboutToStart() {
+
+        // Show Progress bar while downloading.
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    // Set image in layout and hide progress bar.
+    public void onImageDownloadSuccess(Bitmap bitmap) {
+
+        mImageViewPoster.setImageBitmap(bitmap);
+
+        // Hide progress bar.
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+    // Set different images on errors from OMDbImageDownloaderAsyncTask.
+    public void onImageDownloadError(int httpStatusCode, String errorMessage) {
+
+        if (httpStatusCode != HttpURLConnection.HTTP_OK) {
+            mImageViewPoster.setImageResource(R.drawable.movie_clapper_icon);
+        }
+
+        // Hide progress bar.
+        mProgressBar.setVisibility(View.INVISIBLE);
     }
 }
